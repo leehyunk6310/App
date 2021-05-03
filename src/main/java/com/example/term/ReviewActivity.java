d611a8da112f8a7667273282ac7bd24bb0af3c8c
spartty <65155903+leehyunk6310@users.noreply.github.com>
2021-05-03 23:04:52 +0900

    5/4 23:04

--- src/main/java/com/example/term/ReviewActivity.java
+++ src/main/java/com/example/term/ReviewActivity.java
@@ -1,6 +1,11 @@
 package com.example.term;
 
+import android.media.Rating;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,13 @@ public class ReviewActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.review);
+
+        RatingBar ratingBar = (RatingBar)findViewById(R.id.ratingBar);
+        EditText text = (EditText)findViewById(R.id.contents);
+        Button save = (Button)findViewById(R.id.saveButton);
+
+
+
 
     }
 }
